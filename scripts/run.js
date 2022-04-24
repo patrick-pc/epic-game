bb481928af2703561f593b6dbe65900a5f94fb1f
Peter Parker <web3slinger@gmail.com>
2022-04-23 21:36:37 -0500

    Initial commit

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy();
+  await gameContract.deployed();
+  console.log("Contract address:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
