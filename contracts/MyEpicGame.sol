41a30ec0da30de22e21cb3c17e630945fa4e4842
Peter Parker <web3slinger@gmail.com>
2022-04-24 05:27:09 -0500

    Setup data for character NFTs

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,42 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-  constructor() {
-    console.log("gm");
+  struct CharacterAttributes {
+    uint256 characterIndex;
+    string name;
+    string imageURI;
+    uint256 hp;
+    uint256 maxHp;
+    uint256 attackDamage;
+  }
+
+  CharacterAttributes[] defaultCharacters;
+
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint256[] memory characterHp,
+    uint256[] memory characterAttackDmg
+  ) {
+    for (uint256 i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(
+        CharacterAttributes({
+          characterIndex: i,
+          name: characterNames[i],
+          imageURI: characterImageURIs[i],
+          hp: characterHp[i],
+          maxHp: characterHp[i],
+          attackDamage: characterAttackDmg[i]
+        })
+      );
+
+      CharacterAttributes memory c = defaultCharacters[i];
+      console.log(
+        "Done initializing %s w/ HP %s, img %s",
+        c.name,
+        c.hp,
+        c.imageURI
+      );
+    }
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Tanjiro", "Eren", "Luffy"], // Names
+    [
+      "https://i.imgur.com/kWMayHv.gif", // Images
+      "https://i.imgur.com/9dSPBsw.gif",
+      "https://i.imgur.com/kgnnEFy.gif",
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract address:", gameContract.address);
 };
